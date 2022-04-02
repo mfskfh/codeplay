97fdecdaf0ea06c371d50a9f05bdf9b5737f032b
mfskfh <jhyangja5@gmail.com>
2022-04-02 14:29:36 +0900

    line 13 오류 고침

--- pygame/pygame_test.py
+++ pygame/pygame_test.py
@@ -10,7 +10,7 @@ while True:
     screen.fill((0, 0, 0))
 
     event = pygame.event.poll()
-    if event.type == pygame.QUIT():
+    if event.type == pygame.QUIT:
         break
 
     pygame.display.update()
