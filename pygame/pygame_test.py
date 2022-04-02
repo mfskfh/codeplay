919c8899857bae971e4c843f87e98d7b02719909
mfskfh <jhyangja5@gmail.com>
2022-04-02 14:08:02 +0900

    파이게임 기본창 열기 실습

---
+++ pygame/pygame_test.py
@@ -0,0 +1,19 @@
+# -*- conding: utf-8 -*-
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 800))
+clock =  pygame.time.Clock()
+
+while True:
+    screen.fill((0, 0, 0))
+
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT():
+        break
+
+    pygame.display.update()
+    clock.tick(30)
+
+pygame.quit()
