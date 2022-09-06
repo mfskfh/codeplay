86487421aec9b193acb1004b76238bfabb736361
mfskfh <jhyangja5@gmail.com>
2022-09-06 17:08:21 +0900

    즐겁다 햄버거집 9월 6일 test_03 일주일 완성 아마도

--- project/test_03.py
+++ project/test_03.py
@@ -1,8 +1,24 @@
+# -*- coding: utf-8 -*-
+
 import random
 from data_01 import *
 
-choose_things = []
+def percent (a):
+    percent_value = random.randint(1, a)
+    if 0 < percent_value <= a:
+        return True
+    else:
+        return False
+
+satisfaction = 10
+
+passed_guest = 0  
+order_guest = 0
+order_menu = 0
+random_things = 0
 press_key = 0
+
+choose_things = []
 
 money = 10000
 
@@ -10,7 +26,8 @@ passed_days = 0
 passed_guest = 0
 
 while passed_days < 7:
-    while passed_guest < 10:
+    while passed_guest < satisfaction:
+  
         order_guest = 0
         order_menu = 0
         random_things = 0
@@ -46,7 +63,7 @@ while passed_days < 7:
                 else:
                     random_things = random.randint(1, 6)
                 print(random_things)
-                money -= 300
+                money -= 500
             elif press_key == "e":
                 if order_menu[0] == choose_things:
                     print("perfect")
@@ -61,6 +78,7 @@ while passed_days < 7:
                     else:
                         print("ㅈㄴ못만드네")
                         money -= 2000
+                        satisfaction -= 1
             elif press_key != "e" or "f":
                 if press_key == "":
                     choose_things.append(random_things)
@@ -73,9 +91,27 @@ while passed_days < 7:
             print("----------------------")
 
         passed_guest += 1  
+
+        if satisfaction > 10:
+            satisfaction = 10
+
+        if satisfaction <= 0:
+            print("만족도가 너무 하락하여 알바를 고용하였습니다 (만족도 2 상승)")
+            satisfaction = 2
+            money - 10000
+
+        if money < 0:
+            if loan_state == 0:
+                print("돈이 다 떨어져 빚을 졌습니다")
+                loan_state = 1
+        else:
+            loan_state = 0
+
     
+    passed_guest = 0
     passed_days += 1
     print("----------------------")
     print("하루가 지났다 | {0}일차 끝".format(passed_days))
     print("남은돈 : {0}".format(money))
+    print("만족도 : {0}".format(satisfaction))
     print("----------------------")
